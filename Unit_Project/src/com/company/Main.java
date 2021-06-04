d49543853068ac4373f5c949a1dac18b444c9dc7
Greg <sbowles21@basecampcodingacademy.org>
2021-06-03 23:51:54 -0500

    create and edit and beginning delete

--- Unit_Project/src/com/company/Main.java
+++ Unit_Project/src/com/company/Main.java
@@ -174,7 +174,7 @@ public class Main {
                 System.out.print("\nHow many lives do you want to have? ");
                 int lives = myscanner.nextInt();
                 myscanner.nextLine();
-                System.out.print("\n How long do you want te game to last?");
+                System.out.print("\n How long do you want the game to last?");
                 double time = myscanner.nextDouble();
                 Game.add(new Gamestate(title, town, kind, lives, time));
                 for (Gamestate G : Game) {
@@ -182,10 +182,243 @@ public class Main {
                 }
             }
             else if (input == 2) {
+                while(true) {
+                    System.out.print("What would you like to edit? ");
+                    System.out.print("""
+                                                    
+                            1.Title
+                            2.Town
+                            3.Enemy Type
+                            4.What kind of enemy?
+                            5.Lives
+                            6.Time Limit
+                            7.Quit
+                            """);
+                    int change_input = myscanner.nextInt();
+                    myscanner.nextLine();
+                    if (change_input == 1) {
+                        System.out.print("What do you want to name your world?");
+                        String title = myscanner.nextLine();
+                        for(Gamestate G: Game){
+                            G.title = title;
+                        }
+                    }
+                    else if (change_input == 2) {
+                        System.out.print("""
+                        ~What Town??~
+                                            
+                        1.Hollowfair
+                        2.Rusthollow
+                        3.Falsewallow
+                        4.Oxhelm
+                        5.Dravin
+                        """);
+                        System.out.print("\n>");
+                        String town = "";
+                        int tinput = myscanner.nextInt();
+                        myscanner.nextLine();
+                        if (tinput == 1) {
+                            town = "Hollowfair";
+                        }
+                        if (tinput == 2) {
+                            town = "Rusthollow";
+                        }
+                        if (tinput == 3) {
+                            town = "Falsewallow";
+                        }
+                        if (tinput == 4) {
+                            town = "Oxhelm";
+                        }
+                        if (tinput == 5) {
+                            town = "Dravin";
+                        }
+                        for (Gamestate G: Game){
+                            G.town = town;
+                        }
+                    }
+                    else if (change_input == 3){
+                        for (Minion1 m1 : Minion1) {
+                            System.out.print(m1.Name);
+                        }
+                        for (Minion2 m2 : Minion2) {
+                            System.out.print("\n" + m2.Name);
+                        }
+                        for (Boss b : Boss) {
+                            System.out.print("\n" + b.Name);
+                        }
+                        System.out.print("\nChoose enemy by name! ");
+                        String enemy_input = myscanner.nextLine();
+                        String min1 = "";
+                        String min2 = "";
+                        String boss_name = "";
 
-            } else if (input == 3) {
-
-            }else if (input == 4){
+                        for (Minion1 m1 : Minion1) {
+                            min1 = m1.Name;
+                        }
+                        for (Minion2 m2 : Minion2) {
+                            min2 = m2.Name;
+                        }
+                        for (Boss b : Boss) {
+                            boss_name = b.Name;
+                        }
+                        if(enemy_input.equals(min1)){
+                            System.out.print("\nWhat is the name of your minion? ");
+                            System.out.print("\n>");
+                            String Minion1_name = myscanner.nextLine();
+                            System.out.print("\nWhat is their health? 100-1000");
+                            System.out.print("\n>");
+                            int heath1 = myscanner.nextInt();
+                            if (heath1 < 100) {
+                                heath1 = 100;
+                            } else if (heath1 > 1000) {
+                                heath1 = 1000;
+                            }
+                            myscanner.nextLine();
+                            System.out.print("\nWhat weapon will they have?");
+                            System.out.print("""
+                                        
+                        1.Sword
+                        2.Spear
+                        3.Bow and Arrow
+                        """);
+                            System.out.print("\n>");
+                            int weapon1 = myscanner.nextInt();
+                            myscanner.nextLine();
+                            if (weapon1 == 1) {
+                                int random_damage = r.nextInt(25 - 10) + 10;
+                                Minion1.add(new Minion1(Minion1_name, heath1, "Sword", random_damage));
+                            } else if (weapon1 == 2) {
+                                int random_damage = r.nextInt(30 - 15) + 15;
+                                Minion1.add(new Minion1(Minion1_name, heath1, "Spear", random_damage));
+                            } else if (weapon1 == 3) {
+                                int random_damage = r.nextInt(30 - 10) + 10;
+                                Minion1.add(new Minion1(Minion1_name, heath1, "Bow and Arrow", random_damage));
+                            }
+                        }
+                        else if(enemy_input.equals(min2)){
+                            System.out.print("\nWhat is the name of your minion? ");
+                            System.out.print("\n>");
+                            String Minion2_name = myscanner.nextLine();
+                            System.out.print("\nWhat is their health? 100-1000");
+                            System.out.print("\n>");
+                            int heath2 = myscanner.nextInt();
+                            if (heath2 < 100) {
+                                heath2 = 100;
+                            } else if (heath2 > 1000) {
+                                heath2 = 1000;
+                            }
+                            myscanner.nextLine();
+                            System.out.print("\nWhat weapon will they have?");
+                            System.out.print("""
+                                        
+                        1.Sword
+                        2.Spear
+                        3.Bow and Arrow
+                        """);
+                            System.out.print("\n>");
+                            int weapon2 = myscanner.nextInt();
+                            myscanner.nextLine();
+                            if (weapon2 == 1) {
+                                int random_damage = r.nextInt(25 - 10) + 10;
+                                Minion2.add(new Minion2(Minion2_name, heath2, "Sword", random_damage));
+                            } else if (weapon2 == 2) {
+                                int random_damage = r.nextInt(25 - 10) + 10;
+                                Minion2.add(new Minion2(Minion2_name, heath2, "Spear", random_damage));
+                            } else if (weapon2 == 3) {
+                                int random_damage = r.nextInt(25 - 10) + 10;
+                                Minion2.add(new Minion2(Minion2_name, heath2, "Bow and Arrow", random_damage));
+                            }
+                        }
+                        else if(enemy_input.equals(boss_name)){
+                            System.out.print("\nWhat is the name of your Boss? ");
+                            System.out.print("\n>");
+                            String Boss_name = myscanner.nextLine();
+                            System.out.print("\nWhat is their health? 10,000-100,000");
+                            System.out.print("\n>");
+                            int Bossheath = myscanner.nextInt();
+                            if (Bossheath < 10000) {
+                                Bossheath = 10000;
+                            } else if (Bossheath > 100000) {
+                                Bossheath = 100000;
+                            }
+                            myscanner.nextLine();
+                            System.out.print("\nWhat weapon will they have?");
+                            System.out.print("""
+                                        
+                        1.Vindicator's Cleaver
+                        2.Hailstorm Harpoon
+                        3.Mourning Flame
+                        """);
+                            System.out.print("\n>");
+                            int Bossweapon = myscanner.nextInt();
+                            myscanner.nextLine();
+                            if (Bossweapon == 1) {
+                                int random_damage = r.nextInt(150 - 100) + 100;
+                                Boss.add(new Boss(Boss_name, Bossheath, "Sword", random_damage));
+                            } else if (Bossweapon == 2) {
+                                int random_damage = r.nextInt(175 - 125) + 100;
+                                Boss.add(new Boss(Boss_name, Bossheath, "Sword", random_damage));
+                            } else if (Bossweapon == 3) {
+                                int random_damage = r.nextInt(200 - 100) + 100;
+                                Boss.add(new Boss(Boss_name, Bossheath, "Sword", random_damage));
+                            }
+                        }
+                    }
+                    else if (change_input == 4) {
+                        System.out.print("\nWhat kind of enemies? (Write the names)\n ");
+                        for (Minion1 m1 : Minion1) {
+                            System.out.print(m1.Name);
+                        }
+                        for (Minion2 m2 : Minion2) {
+                            System.out.print("\n" + m2.Name);
+                        }
+                        for (Boss b : Boss) {
+                            System.out.print("\n" + b.Name);
+                        }
+                        System.out.print("\n>");
+                        String kind = myscanner.nextLine();
+                        for(Gamestate G: Game){
+                            G.what_kind = kind;
+                        }
+                    }
+                    else if (change_input == 5) {
+                        System.out.print("\nHow many lives do you want to have? ");
+                        int lives = myscanner.nextInt();
+                        myscanner.nextLine();
+                        for(Gamestate G: Game){
+                            G.lives = lives;
+                        }
+                    }
+                    else if (change_input == 6) {
+                        System.out.print("\n How long do you want te game to last?");
+                        double time = myscanner.nextDouble();
+                        for(Gamestate G: Game){
+                            G.time_limit = time;
+                        }
+                    }
+                    else if (change_input == 7){
+                        break;
+                    }
+                }
+            }
+            else if (input == 3) {
+                for (Gamestate G: Game){
+                    System.out.print("What Game would you like to delete(Enter the name)");
+                    for(int i = 0; i < Game.size(); i += 1) {
+                        System.out.print("\n"+G.title);
+                    }
+                    System.out.print("\n>");
+                    String Del_input = myscanner.nextLine();
+                    if (Del_input.equals(G.title)){
+                        if (Game.size() == 0) {
+                            System.out.println("You have no contacts!");
+                        } else {
+                            Game.removeIf(Gamestate -> Del_input.equalsIgnoreCase(G.title));
+                        }
+                    }
+                }
+            }
+            else if (input == 4){
                 break;
             }
         }
